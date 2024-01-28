a5f1dc3a546b8c971f9e821f05ea124ac8c100da
Lira Guedes <liracrg@gmail.com>
2024-01-28 20:45:55 -0300

    test: Create test for the Product object constructor/initializer

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,3 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
-# iniciando projeto
+    product = Product(
+        id="7013",
+        product_name="veniam",
+        company_name="Cunha",
+        manufacturing_date="2023-06-13",
+        expiration_date="2052-10-23",
+        serial_number="QS71 VBJ6 9851 8Q7P E4D4 JNYC 3X25",
+        storage_instructions="Facere quisquam exercitationem laudantium qui. Praesentium ut praesentium laudantium unde officiis praesentium.",
+    )
+
+    assert product.id == "7013"
+    assert product.product_name == "veniam"
+    assert product.company_name == "Cunha"
+    assert product.manufacturing_date == "2023-06-13"
+    assert product.expiration_date == "2052-10-23"
+    assert product.serial_number == "QS71 VBJ6 9851 8Q7P E4D4 JNYC 3X25"
+    assert product.storage_instructions == "Facere quisquam exercitationem laudantium qui. Praesentium ut praesentium laudantium unde officiis praesentium."
+
