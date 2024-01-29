0d2694a58ae2b86af3526fb5acf304027a226568
Lira Guedes <liracrg@gmail.com>
2024-01-28 21:56:33 -0300

    feat: Create Inventory class

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self._data = data if data is not None else []
+
+    def add_data(self, data: List[Product]):
+        self._data.extend(data)
+
+    # propriedade apenas para leitura que retorna cópia da lista de produtos
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
